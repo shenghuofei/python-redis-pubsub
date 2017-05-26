8da0f25a4fbb8b22d15021422680557cda44c075
wangfy <wangfy@fenbi.com>
2017-05-26 10:01:11 +0800

    init

--- sub.py
+++ sub.py
@@ -6,7 +6,7 @@ rc = redis.Redis(host='127.0.0.1')
   
 ps = rc.pubsub()  
   
-ps.subscribe(['channel1', 'channel2'])  #订阅两个频道，分别是foo，或bar  
+ps.subscribe(['channel1', 'channel2'])  #订阅两个频道，分别是channel1，或channel2  
   
 for item in ps.listen(): 
     print item 
